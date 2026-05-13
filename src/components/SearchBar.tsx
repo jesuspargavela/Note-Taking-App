6985342e5700484fdc5ef6c96fa5a33b843dc4c4
jesuspargavela <jesuspargavel@gmail.com>
2026-05-12 22:19:31 -0600

    add search bar logic

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,37 +1,77 @@
+import { useState } from "react";
+
+import { useNotes } from "../hooks/useNotes";
+
 import "./search-bar.css";
 
 function SearchBar() {
+  const [query, setQuery] = useState<string>("");
+  const { notes, archivedNotes, setSearchedBySearchBarNotes, setNoteTypes } =
+    useNotes();
+
+  const handleSubmit = (event: React.SubmitEvent) => {
+    event.preventDefault();
+
+    if (!query) return;
+
+    setSearchedBySearchBarNotes([]);
+
+    const mergedNotes = [...new Set([...notes, ...archivedNotes])];
+
+    for (const note of mergedNotes) {
+      if (
+        note.title.toLowerCase().includes(query.toLowerCase()) ||
+        note.content.toLowerCase().includes(query.toLowerCase())
+      ) {
+        setSearchedBySearchBarNotes((prev) => [...new Set([...prev, note])]);
+      }
+    }
+
+    for (const note of mergedNotes) {
+      note.tags.forEach((t: string) => {
+        if (t.toLowerCase() === query.toLowerCase()) {
+          setSearchedBySearchBarNotes((prev) => [...new Set([...prev, note])]);
+        }
+      });
+    }
+
+    setNoteTypes("SearchBar");
+  };
+
   return (
     <div className="flex items-center">
-      <span className="absolute left-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="17"
-          height="17"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="white"
-            fill-rule="evenodd"
-            d="M11.248 3.5a7.289 7.289 0 1 0 0 14.577 7.289 7.289 0 0 0 0-14.577ZM2.46 10.79a8.789 8.789 0 1 1 17.577 0 8.789 8.789 0 0 1-17.577 0Z"
-            clip-rule="evenodd"
-          />
-          <path
-            fill="white"
-            fill-rule="evenodd"
-            d="m16.736 15.648 5.616 5.6-1.06 1.063-5.615-5.601 1.06-1.062Z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </span>
-      <input
-        className="w-74 rounded-sm border border-white pt-1 pb-1 pl-8 placeholder:text-white"
-        type="search"
-        name="search-bar"
-        id="search-bar"
-        placeholder="Search by title, content, or tags..."
-      />
+      <form onSubmit={handleSubmit}>
+        <span className="absolute top-6 left-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="17"
+            height="17"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="white"
+              fill-rule="evenodd"
+              d="M11.248 3.5a7.289 7.289 0 1 0 0 14.577 7.289 7.289 0 0 0 0-14.577ZM2.46 10.79a8.789 8.789 0 1 1 17.577 0 8.789 8.789 0 0 1-17.577 0Z"
+              clip-rule="evenodd"
+            />
+            <path
+              fill="white"
+              fill-rule="evenodd"
+              d="m16.736 15.648 5.616 5.6-1.06 1.063-5.615-5.601 1.06-1.062Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+        <input
+          className="w-74 rounded-sm border border-white pt-1 pb-1 pl-8 placeholder:text-white"
+          type="search"
+          name="query"
+          id="query"
+          placeholder="Search by title, content, or tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
